cb8930cfba7177b35626bcdbce1d7a73d878821e
bigr4232 <ryanmf4232@gmail.com>
2023-07-15 02:48:54 -0700

    command finished to add user

--- bot-main/clash_war_pull.py
+++ bot-main/clash_war_pull.py
@@ -1,24 +1,26 @@
 import coc
 import asyncio
 import discord
+from discord import app_commands
 import config_loader
 
 players = list()
 clan_tags = list()
+initContent = config_loader.loadYaml()
 
 # Intents and tree inits
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
 managers = {}
+tree = app_commands.CommandTree(bot)
 
 # Event for new war start. Will start the war attack notifier
 @coc.WarEvents.new_war(tags=clan_tags)
 async def new_war(war):
     print('new war registered')
-    content = config_loader.loadYaml()
     for member in war.members:
-        if member.clan.tag == content['discordChannel']:
+        if member.clan.tag == int(initContent['discordChannel']):
             players.append(member)
     war_notifier(war)
 
@@ -41,40 +43,55 @@ async def war_notifier(war):
             asyncio.sleep(86400)
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 68400)
+        content = config_loader.loadYaml()
         for member in players:
             print('5 hours left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 7200)
+        content = config_loader.loadYaml()
         for member in players:
             print('3 hours left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 3600)
+        content = config_loader.loadYaml()
         for member in players:
-            print('1 and a half hours left')
+            print('1 hour 30 minutes left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 2400)
+        content = config_loader.loadYaml()
         for member in players:
             print('1 hour left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 1200)
+        content = config_loader.loadYaml()
         for member in players:
             print('30 minutes left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 600)
+        content = config_loader.loadYaml()
         for member in players:
             print('15 minutes left')
+
+@tree.command(name='claimaccount', description='claim clash account with tag and discord name', guild=discord.Object(id=int(initContent['discordGuildID'])))
+async def claimAccountCommand(ctx: discord.Interaction, clashtag:str, discordusername:str=''):
+    await ctx.response.send_message("message")
+    if discordusername != '':
+        config_loader.addUser(discordusername, clashtag)
+    else:
+        config_loader.addUser(ctx.user.name, clashtag)
+    
 
 # Bot init
 @bot.event
 async def on_ready():
-    print("connected")
+    await tree.sync(guild=discord.Object(id=int(initContent['discordGuildID'])))
+    print("synced commands")
 
 # coc API init
 async def main():
     async with coc.EventsClient() as coc_client:
-        content = config_loader.loadYaml()
         try:
-            await coc_client.login_with_tokens(content['clashToken'])
+            await coc_client.login_with_tokens(initContent['clashToken'])
         except coc.InvalidCredentials as error:
             exit(error)
         coc_client.add_clan_updates(*clan_tags)
@@ -83,12 +100,15 @@ async def main():
             new_war,
             war_attack
         )
-        clan_tags.append(content['clanTag'])
+        clan_tags.append(initContent['clanTag'])
 
         # Add the client session to the bot
         bot.coc_client = coc_client
-        await bot.start(content['discordBotToken'])
+        await bot.start(initContent['discordBotToken'])
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
