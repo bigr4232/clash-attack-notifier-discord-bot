8aa22ec6c69cdf4e687f9bb515ef3e1fece54005
bigr4232 <ryanmf4232@gmail.com>
2023-07-16 01:08:23 -0700

    Changed frequency of re-loading config

--- bot-main/clash_war_pull.py
+++ bot-main/clash_war_pull.py
@@ -4,9 +4,11 @@ import discord
 from discord import app_commands
 import config_loader
 
+# Globals
 players = list()
 clan_tags = list()
-initContent = config_loader.loadYaml()
+content = config_loader.loadYaml()
+commandsSynced = False
 
 # Intents and tree inits
 intents = discord.Intents.default()
@@ -20,7 +22,7 @@ tree = app_commands.CommandTree(bot)
 async def new_war(war):
     print('new war registered')
     for member in war.members:
-        if member.clan.tag == int(initContent['discordChannel']):
+        if member.clan.tag == int(content['discordChannel']):
             players.append(member)
     war_notifier(war)
 
@@ -43,56 +45,57 @@ async def war_notifier(war):
             asyncio.sleep(86400)
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 68400)
-        content = config_loader.loadYaml()
         for member in players:
             print('5 hours left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 7200)
-        content = config_loader.loadYaml()
         for member in players:
             print('3 hours left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 3600)
-        content = config_loader.loadYaml()
         for member in players:
             print('1 hour 30 minutes left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 2400)
-        content = config_loader.loadYaml()
         for member in players:
             print('1 hour left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 1200)
-        content = config_loader.loadYaml()
         for member in players:
             print('30 minutes left')
         war = await cc.get_current_war(clan_tags[0])
         asyncio.sleep(war.end_time.seconds_until - 600)
-        content = config_loader.loadYaml()
         for member in players:
             print('15 minutes left')
 
 # Command to claim clash account. With no input of username, will use discord name from command issuer
-@tree.command(name='claimaccount', description='claim clash account with tag and discord name', guild=discord.Object(id=int(initContent['discordGuildID'])))
+@tree.command(name='claimaccount', description='claim clash account with tag and discord name', guild=discord.Object(id=int(content['discordGuildID'])))
 async def claimAccountCommand(ctx: discord.Interaction, clashtag:str, discordusername:str=''):
-    await ctx.response.send_message("message")
     if discordusername != '':
+        await ctx.response.send_message(f"Claiming account {clashtag} for {discordusername}")
         config_loader.addUser(discordusername, clashtag)
     else:
+        await ctx.response.send_message(f"Claiming account {clashtag} for {ctx.user.name}")
         config_loader.addUser(ctx.user.name, clashtag)
+    global content
+    content = config_loader.loadYaml()
     
 
 # Bot init
 @bot.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=int(initContent['discordGuildID'])))
-    print("synced commands")
+    global commandsSynced
+    if commandsSynced == False:
+        await tree.sync(guild=discord.Object(id=int(content['discordGuildID'])))
+        commandsSynced = True
+        print('commands synced')
+    print('ready')
 
 # coc API init
 async def main():
     async with coc.EventsClient() as coc_client:
         try:
-            await coc_client.login_with_tokens(initContent['clashToken'])
+            await coc_client.login_with_tokens(content['clashToken'])
         except coc.InvalidCredentials as error:
             exit(error)
         coc_client.add_clan_updates(*clan_tags)
@@ -101,11 +104,11 @@ async def main():
             new_war,
             war_attack
         )
-        clan_tags.append(initContent['clanTag'])
+        clan_tags.append(content['clanTag'])
 
         # Add the client session to the bot
         bot.coc_client = coc_client
-        await bot.start(initContent['discordBotToken'])
+        await bot.start(content['discordBotToken'])
 
 
 if __name__ == "__main__":
