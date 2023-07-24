29eb26ac463fda40851432678af1b2711dfd0b94
bigr4232 <ryanmf4232@gmail.com>
2023-07-24 00:18:37 -0700

    Remove unecessary file

--- bot-main/account_link.py
+++
@@ -1,9 +0,0 @@
-class account:
-    def __init__(self, discordID):
-        self.discordID = discordID
-        self.clashTags = set()
-
-    def addClashTag(self, clashTag):
-        self.clashTags.add(clashTag)
-
-    
